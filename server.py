e31a47b1f0e239989503da0965fe1f4bf84446a7
AvalonLucero <avalonlm15@gmail.com>
2025-03-04 23:16:52 -0500

    Finished project

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
+"""
+Emotion Detection Server made by Avalon Munoz
+Server.py is to allow us to use Flask
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
-
 @app.route("/")
 def render_index_page():
+    """
+    Renders the main page
+    """
     return render_template('index.html')
-
 
 @app.route("/emotionDetector")
 def emote_detect():
+    """
+    Function to analyze the emotion of a text
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -25,7 +33,9 @@ def emote_detect():
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is 'anger': {},'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger,disgust,fear,joy,sadness,dominant_emotion)
+    return f"For the given statement, the system response is 'anger': {anger},\
+            'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.\
+            The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
